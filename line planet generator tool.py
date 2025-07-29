bffc0d1273702474170c13bf542cb6cb0f07031f
Alex <awerth2k24@gmail.com>
2025-07-29 01:20:09 -0400

    DIAGETIC LINE COLLISIONS
    Push to prod baby! As far as I can test, collisions with line planets are now defined live and are defined properly according to the radius of the smallbody colliding with them. Magnificent!

---
+++ line planet generator tool.py
@@ -0,0 +1,22 @@
+# Spacefruit bonus tool: line planet generator machine
+# Alex Werth
+# 7 / 29 / 25
+
+from cmu_graphics import *
+
+def onAppStart(app):
+    pass
+
+def onMousePress(app, mouseX, mouseY):
+    pass
+
+def onMouseMove(app, mouseX, mouseY):
+    pass
+
+def redrawAll(app):
+    pass
+
+def main():
+    runApp(width = 600, height = 600)
+
+main()
