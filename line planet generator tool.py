9a7f1275b1b3eb1c866b077ce13f19dd02ab0a58
Alex <awerth2k24@gmail.com>
2025-07-30 21:11:08 -0400

    Transparent sprites, first planets, velocity indicator
    The time approaches! I am getting concerned that a few things are.... unlikely to make it into this game. However, most things work now!
    
    - Transparent sprites
    - Somber heart and bramble triplets
    - Diagetic landing mode
    - BEAUTIFUL velocity indicator
    
    Things still to do
    - fruit :(
    - probably simple fruit spawners
    - player fruit collection in some manner
    - score system for depositing the fruit in a bucket on the home planet
    
    - player gravity moves downwards to bottom of screen (dear god this one will haunt me)
    - other meaningful indicator of where gravitational bodies are
    
    OOOH and a parallax star background. That seems like it might be a doozy. Do we suppose my strategy still, giving it 99% of the player's (linear) velocity in worldspace, will work? I should try it.
    
    Oh well. How fun it all is, though! And i suppose tomorrow, I get to bring it together artistically.

--- line planet generator tool.py
+++ line planet generator tool.py
@@ -5,16 +5,58 @@
 from cmu_graphics import *
 
 def onAppStart(app):
-    pass
+    app.scale = 1
+    #app.points = [
+    #    (100, 100),
+    #    (-100, 100),
+    #    (-200, -200),
+    #    (200, -100)
+    #]
+    app.pointsSomberHeart = [
+        (300, 300), # top inner lip
+        (400, 500), (0, 600), (-350, 500), # very top
+        (-500, 300), (-600, 0), (-500, -300), # back plate
+        (-350, -500), (100, -650), (500, -500), # very bottom
+        (400, -300), # bottom inner lip
+        (0, -400), (-300, -250), (-400, 0), (-300, 250), (0, 400) # interior
+    ]
 
-def onMousePress(app, mouseX, mouseY):
-    pass
+    app.points = [
+        (100, 100),
+        (-200, 300),
+        (-200, -300)
+    ]
 
-def onMouseMove(app, mouseX, mouseY):
-    pass
+def onKeyPress(app, key):
+    if key == 'up':
+        app.scale += 0.1
+    if key == 'down':
+        app.scale -= 0.1
+
+
+# DRAW
+
+def turnToScreenSpace(app, point):
+    return( point[0] * app.scale + app.width / 2, point[1] * -app.scale + app.height / 2)
+
 
 def redrawAll(app):
-    pass
+    drawLabel(f'Up and down arrows to change scale, currently {app.scale}', app.width / 2, 40)
+
+    drawCircle(app.width / 2, app.height / 2, 5, fill = 'black')
+
+    for pointIndex in range(len(app.points)):
+        thisPoint = app.points[pointIndex]
+        prevPoint = app.points[pointIndex - 1]
+
+        color = 'red' if pointIndex == 0 else 'blue'
+
+        drawLine(*turnToScreenSpace(app, prevPoint), *turnToScreenSpace(app, thisPoint), lineWidth = 2 * app.scale, fill = 'orchid', arrowEnd = True)
+
+        drawCircle(*turnToScreenSpace(app, thisPoint), 5 * app.scale, fill = color)
+
+    debugMainSprite = CMUImage.open("assets/TRANSPARENCY TEST SHUTTLE-01.png")
+    drawImage(debugMainSprite, app.width / 2, app.height / 2)
 
 def main():
     runApp(width = 600, height = 600)
