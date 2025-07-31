7ca0f6d47bda546fae48f08f575acb649414ec84
Alex <awerth2k24@gmail.com>
2025-07-31 15:00:04 -0400

    FINAL VERSION: Prettiest It Can Be
    After running into some final problems (check out the ruby and sapphire rings- their sprites have the most fascinating oscillation problem) and circumventing them with, surprisingly, some tasteful cmu graphics implementation, I am ready to call it here.
    
    The game works! Fly in space: find and collect fruit. Hell, it's extra tactile: bring it back to the one spot on the home planet to haul it off, and watch the numbers go up. Not too bad, frankly.
    
    Sprites are all my cutesy little designs. Much obliged.
    
    Urgh. Video onwards, one supposes.

--- line planet generator tool.py
+++ line planet generator tool.py
@@ -27,10 +27,20 @@ def onAppStart(app):
         (-200, -300)
     ]
 
+    app.pointsSteelOrchard = [
+        (-900, 1200), # Bottom left corner intersecting with the planet (radius 1500, 3-4-5 triangle)
+        (-950, 1400),
+
+        (950, 1400),
+        (900, 1200)# Bottom right corner back at planet radius
+    ]
+
+    app.meshesToDraw = [app.pointsSomberHeart]
+
 def onKeyPress(app, key):
     if key == 'up':
         app.scale += 0.1
-    if key == 'down':
+    if key == 'down' and app.scale > 0.2:
         app.scale -= 0.1
 
 
@@ -44,19 +54,21 @@ def redrawAll(app):
     drawLabel(f'Up and down arrows to change scale, currently {app.scale}', app.width / 2, 40)
 
     drawCircle(app.width / 2, app.height / 2, 5, fill = 'black')
+    for thisMesh in app.meshesToDraw:
+        for pointIndex in range(len(thisMesh)):
+            thisPoint = thisMesh[pointIndex]
+            prevPoint = thisMesh[pointIndex - 1]
 
-    for pointIndex in range(len(app.points)):
-        thisPoint = app.points[pointIndex]
-        prevPoint = app.points[pointIndex - 1]
+            color = 'red' if pointIndex == 0 else 'blue'
 
-        color = 'red' if pointIndex == 0 else 'blue'
+            drawLine(*turnToScreenSpace(app, prevPoint), *turnToScreenSpace(app, thisPoint), lineWidth = 2 * app.scale, fill = 'orchid', arrowEnd = True)
 
-        drawLine(*turnToScreenSpace(app, prevPoint), *turnToScreenSpace(app, thisPoint), lineWidth = 2 * app.scale, fill = 'orchid', arrowEnd = True)
-
-        drawCircle(*turnToScreenSpace(app, thisPoint), 5 * app.scale, fill = color)
-
-    debugMainSprite = CMUImage.open("assets/TRANSPARENCY TEST SHUTTLE-01.png")
-    drawImage(debugMainSprite, app.width / 2, app.height / 2)
+            drawCircle(*turnToScreenSpace(app, thisPoint), 5 * app.scale, fill = color)
+    
+    #rubySprite = 'assets/planets/Sapphire Bramble.png'
+    #rubyW, rubyH = getImageSize(rubySprite)
+    #drawImage(rubySprite, 300, 300, width = rubyW, height = rubyH, align = 'center')
+    #drawImage(rubySprite, 300, 300, width = rubyW, height = rubyH, align = 'center', rotateAngle = 45)
 
 def main():
     runApp(width = 600, height = 600)
